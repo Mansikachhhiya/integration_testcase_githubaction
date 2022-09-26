3cc8ad1f65db5dc5b26015dd527120e78edfa500
Mansikachhhiya <kachhiyamanshi1542810@gmail.com>
2022-09-26 17:32:02 +0530

    Delete user feature added

--- test/route.test.js
+++ test/route.test.js
@@ -1,11 +1,11 @@
-// const request = require("supertest");
-// const app = require("../index");
+const request = require("supertest");
+const app = require("../index");
 
-// describe("User Api test",()=> {
-   //  it("should delete a user",async ()=>{
-   //      const res = await request(app).delete("/api/users/1")
-   //      expect(res.statusCode).toEqual(204)
-   //  })
+describe("User Api test",()=> {
+    it("should delete a user",async ()=>{
+        const res = await request(app).delete("/api/users/1")
+        expect(res.statusCode).toEqual(204)
+    })
    //
    //      it("Should create a new user ", async () => {
    //          const res = await request(app).post("/api/users")
@@ -28,6 +28,6 @@
    //      expect(res.statusCode).toEqual(200)
    //      expect(res.body).toHaveProperty("Users")
    //  })
-   // }
+   }
 
-// )
+)
